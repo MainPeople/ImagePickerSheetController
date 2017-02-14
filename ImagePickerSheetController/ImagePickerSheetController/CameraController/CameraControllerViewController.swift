271cbbfe33d0a4618bbf4adbba8824d986c149ec
Alexsander Khitev <Alexsander@MacBook-Pro.local>
2017-02-14 13:26:58 +0400

    added images and button for control camera

--- ImagePickerSheetController/ImagePickerSheetController/CameraController/CameraControllerViewController.swift
+++ ImagePickerSheetController/ImagePickerSheetController/CameraController/CameraControllerViewController.swift
@@ -16,6 +16,9 @@ class CameraControllerViewController: UIViewController {
     private let bottomBar = UIView()
     private let topBar = UIView()
     private let cameraPreviewView = UIView()
+    private let shotButton = UIButton(type: .system)
+    private let cancelButton = UIButton(type: .system)
+    private let switchCameraButton = UIButton(type: .system)
     
     var cameraLayer = AVCaptureVideoPreviewLayer()
 
@@ -27,6 +30,8 @@ class CameraControllerViewController: UIViewController {
         setupUISettings()
         addUIElements()
         setupViewsSettings()
+        // UI Elements settings
+        setupButtonsSettings()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -91,6 +96,13 @@ class CameraControllerViewController: UIViewController {
         view.addSubview(topBar)
         cameraPreviewView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(cameraPreviewView)
+        // buttons
+        shotButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomBar.addSubview(shotButton)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomBar.addSubview(cancelButton)
+        switchCameraButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomBar.addSubview(switchCameraButton)
     }
     
     private func setupUIElementsPositions() {
@@ -104,15 +116,52 @@ class CameraControllerViewController: UIViewController {
         topBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         topBar.heightAnchor.constraint(equalToConstant: 44).isActive = true
         
-        
         cameraPreviewView.bottomAnchor.constraint(equalTo: bottomBar.topAnchor).isActive = true
         cameraPreviewView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        
-        // test
         cameraPreviewView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         cameraPreviewView.topAnchor.constraint(equalTo: self.topBar.bottomAnchor).isActive = true
         
+        // buttons
+        shotButton.centerXAnchor.constraint(equalTo: bottomBar.centerXAnchor).isActive = true
+        shotButton.centerYAnchor.constraint(equalTo: bottomBar.centerYAnchor).isActive = true
+        shotButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        shotButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        cancelButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        cancelButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: bottomBar.centerYAnchor).isActive = true
+        cancelButton.leftAnchor.constraint(equalTo: bottomBar.leftAnchor, constant: 20).isActive = true
+        
+        switchCameraButton.centerYAnchor.constraint(equalTo: bottomBar.centerYAnchor).isActive = true
+        switchCameraButton.rightAnchor.constraint(equalTo: bottomBar.rightAnchor, constant: -20).isActive = true
+        switchCameraButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        switchCameraButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
+    
+    // MARK: - Animation
+    
+    private func animateCameraView() {
+        let widthValue = UIScreen.main.bounds.width
+        let heightValue = UIScreen.main.bounds.height
+        
+        var setupWidthValue: CGFloat!
+        var setupHeightValue: CGFloat!
+        
+        if widthValue < heightValue {
+            setupWidthValue = widthValue
+            setupHeightValue = heightValue - 44 - 96
+        } else {
+            setupWidthValue = heightValue
+            setupHeightValue = widthValue - 44 - 96
+        }
+        
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(0.5)
+        self.cameraLayer.frame = CGRect(x: 0, y: 0, width: setupWidthValue, height: setupHeightValue)
+        CATransaction.commit()
+    }
+    
+    // MARK: - UI Elements settings
     
     private func setupViewsSettings() {
         bottomBar.backgroundColor = .black
@@ -140,27 +189,22 @@ class CameraControllerViewController: UIViewController {
         cameraPreviewView.layer.addSublayer(cameraLayer)
     }
     
-    // MARK: - Animation
-    
-    private func animateCameraView() {
-        let widthValue = UIScreen.main.bounds.width
-        let heightValue = UIScreen.main.bounds.height
+    private func setupButtonsSettings() {
+        let bundle = Bundle(identifier: "com.SCImagePickerSheetController")
+        let shotImage = UIImage(named: "ShotCameraIcon", in: bundle, compatibleWith: nil)
+        shotButton.setImage(shotImage, for: .normal)
         
-        var setupWidthValue: CGFloat!
-        var setupHeightValue: CGFloat!
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.setTitleColor(.white, for: .normal)
+        cancelButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        cancelButton.titleLabel?.adjustsFontSizeToFitWidth = true
         
-        if widthValue < heightValue {
-            setupWidthValue = widthValue
-            setupHeightValue = heightValue - 44 - 96
-        } else {
-            setupWidthValue = heightValue
-            setupHeightValue = widthValue - 44 - 96
-        }
         
-        CATransaction.begin()
-        CATransaction.setAnimationDuration(0.5)
-        self.cameraLayer.frame = CGRect(x: 0, y: 0, width: setupWidthValue, height: setupHeightValue)
-        CATransaction.commit()
+        let switchIcon = UIImage(named: "SwitchCameraIcon", in: bundle, compatibleWith: nil)
+        switchCameraButton.setImage(switchIcon, for: .normal)
+        switchCameraButton.tintColor = .white
     }
+    
+  
     
 }
